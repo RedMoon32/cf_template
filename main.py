4cb5a9d5b60d98eb85b89c35e41ee4cfa3d1509e
RedMoon32 <rinat.babichev@gmail.com>
2024-12-30 12:53:56 +0300

    correct iteration

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ template <class T>
 using pqueue = priority_queue<T, vector<T>, greater<T>>;
 
 // Iteration
-#define RANGE(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a, b) for (int i = (a); i < (b); ++i)
 #define F0R(i, a) FOR(i, 0, a)
 #define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
 #define R0F(i, a) ROF(i, 0, a)
